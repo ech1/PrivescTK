ca1e7a26eeb490be256859d9e9ce80acd28eb329
ech1 <ech1@nowhere.com>
2020-07-07 12:31:02 +0200

    update script

--- privesc.sh
+++ privesc.sh
@@ -51,34 +51,39 @@ echo '[+] 4) pspy64s'
 echo '[+] 5) pspy32s'
 echo ''
 read choice3
-
+cd scripts
 case "$choice3" in
 "1")
-	sh scripts/linpeas.sh
+	chmod +x linpeas.sh
+	./linpeas.sh
 	;;
 "2")
-	sh scripts/pspy64
+	chmod +x pspy64
+	./pspy64
 	;;
 "3")
-	sh scripts/pspy32
+	chmod +x pspy32
+	./pspy32
 	;;
 "4")
-	sh scripts/pspy64s
+	chmod +x pspy64s
+	./pspy64s
 	;;
 "5")
-	sh scripts/pspy32s
+	chmod +x pspy32s
+	./pspy32s
 	;;
 *)
-	echo '[+] Invalid'
+	echo '[+] Serving HTTP Service:'
+	ip a | grep inet | cut -d ' ' -f6
+	echo '[+]which port ?'
+	read portnumber
+	cd ..
+	python -m http.server $portnumber
+	python3 -m http.server $portnumber
 	;;
 esac
 
 
 
 
-if [ $choice3 -eq "0" ]; then
-	echo '[+] Here are the available ips:'
-	ip a | grep inet | cut -d ' ' -f6
-	echo '[+] which port ?'
-	read portnumber
-fi
